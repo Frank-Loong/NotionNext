7dd501b5f555ed52388c1d0a5d5c5049ed237697
Frank <kl2529073273@gmail.com>
2025-06-27 14:23:33 +0800

    feat: 添加Nobelium主题页脚链接区块，优化页脚信息展示

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -1,6 +1,12 @@
 import DarkModeButton from '@/components/DarkModeButton'
-import Vercel from '@/components/Vercel'
 import { siteConfig } from '@/lib/config'
+import AnalyticsBusuanzi from '@/components/AnalyticsBusuanzi'
+import { BeiAnGongAn } from '@/components/BeiAnGongAn'
+import BeiAnSite from '@/components/BeiAnSite'
+import LazyImage from '@/components/LazyImage'
+import PoweredBy from '@/components/PoweredBy'
+import { useGlobal } from '@/lib/global'
+import Link from 'next/link'
 
 export const Footer = (props) => {
   const d = new Date()
@@ -9,21 +15,57 @@ export const Footer = (props) => {
   const fullWidth = post?.fullWidth ?? false
   const since = siteConfig('SINCE')
   const copyrightDate = parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
+  const { siteInfo } = useGlobal()
+  const NOBELIUM_FOOTER_LINKS = siteConfig('NOBELIUM_FOOTER_LINKS', [])
 
   return <footer
      className={`z-10 relative mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${
        !fullWidth ? 'max-w-2xl px-4' : 'px-4 md:px-24'
      }`}
    >
-     <DarkModeButton className='text-center py-4'/>
      <hr className="border-gray-200 dark:border-gray-600" />
-     <div className="my-4 text-sm leading-6">
-       <div className="flex align-baseline justify-between flex-wrap">
-         <p>
-           © {siteConfig('AUTHOR')} {copyrightDate}
-         </p>
-         <Vercel />
+     
+     {/* 信息与链接区块 */}
+     <div className='w-full flex flex-col md:flex-row justify-between py-6'>
+       <div className='gap-y-2 flex flex-col items-start'>
+         <div className='flex gap-x-1 items-center'>
+           {siteInfo?.icon && (
+             <LazyImage
+               src={siteInfo.icon}
+               className='rounded-full'
+               width={24}
+               alt={siteConfig('AUTHOR')}
+             />
+           )}
+           <span className='font-bold'>{siteConfig('AUTHOR')}</span>
+         </div>
+         <div className='px-1'>{siteConfig('DESCRIPTION')}</div>
+         {siteConfig('CONTACT_EMAIL') && (
+           <div className='px-1'>{siteConfig('CONTACT_EMAIL')}</div>
+         )}
        </div>
+     </div>
+     
+     {/* 页脚底部信息 */}
+     <div className='py-4 flex flex-col md:flex-row justify-between items-center border-t border-gray-200 dark:border-gray-600'>
+       <div className='flex gap-x-2 flex-wrap justify-between items-center'>
+         <span>© {siteConfig('AUTHOR')} {copyrightDate}</span>
+         <PoweredBy />
+       </div>
+
+       <DarkModeButton className='my-4 md:my-0'/>
+
+       <div className='flex justify-between items-center gap-x-2'>
+         <div className='flex items-center gap-x-4'>
+           <AnalyticsBusuanzi />
+         </div>
+       </div>
+     </div>
+
+     {/* 备案 */}
+     <div className='w-full text-center flex flex-wrap items-center justify-center gap-x-2 pb-4'>
+       <BeiAnSite />
+       <BeiAnGongAn />
      </div>
    </footer>
 }
