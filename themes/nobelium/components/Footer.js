37b7aeb5963bd428b797a7da70b7eed7a467f009
Frank <kl2529073273@gmail.com>
2025-06-27 15:17:38 +0800

    feat: 优化Nobelium主题页脚布局，统一内容展示并增强可读性

--- themes/nobelium/components/Footer.js
+++ themes/nobelium/components/Footer.js
@@ -23,10 +23,11 @@ export const Footer = (props) => {
    >
      <hr className="border-gray-200 dark:border-gray-600" />
      
-     {/* 信息与链接区块 */}
-     <div className='w-full flex flex-col md:flex-row justify-between py-6'>
-       <div className='gap-y-2 flex flex-col items-start'>
-         <div className='flex gap-x-1 items-center'>
+     {/* 统一的页脚内容 */}
+     <div className='w-full py-6'>
+       <div className='flex flex-col items-center gap-y-4'>
+         {/* 站点信息 */}
+         <div className='flex items-center gap-x-2'>
            {siteInfo?.icon && (
              <LazyImage
                src={siteInfo.icon}
@@ -37,32 +38,32 @@ export const Footer = (props) => {
            )}
            <span className='font-bold'>{siteConfig('TITLE')}</span>
          </div>
-         <div className='px-1'>{siteConfig('DESCRIPTION')}</div>
+         
+         {/* 站点描述 */}
+         <div className='text-center max-w-xl'>{siteConfig('DESCRIPTION')}</div>
          
          {/* 社交媒体按钮 */}
          <SocialButton />
-       </div>
-     </div>
-     
-     {/* 页脚底部信息 */}
-     <div className='py-4 flex flex-col md:flex-row justify-between items-center border-t border-gray-200 dark:border-gray-600'>
-       <div className='flex gap-x-2 flex-wrap justify-between items-center'>
-         <span><i className='mx-1 animate-pulse fas fa-heart'></i> {siteConfig('AUTHOR')} <i className='fas fa-copyright mr-1'></i> {copyrightDate}</span>
-       </div>
-
-       <DarkModeButton className='my-4 md:my-0'/>
-
-       <div className='flex justify-between items-center gap-x-2'>
-         <div className='flex items-center gap-x-4'>
+         
+         {/* 版权信息 */}
+         <div className='flex items-center gap-x-1 mt-2'>
+           <i className='animate-pulse fas fa-heart text-red-500'></i>
+           <i className='fas fa-copyright mx-1'></i>
+           <span>{copyrightDate}</span>
+         </div>
+         
+         {/* 功能按钮和统计 */}
+         <div className='flex flex-wrap justify-center items-center gap-x-4 gap-y-2'>
+           <DarkModeButton />
            <AnalyticsBusuanzi />
          </div>
+         
+         {/* 备案信息 */}
+         <div className='flex flex-wrap justify-center items-center gap-x-2'>
+           <BeiAnSite />
+           <BeiAnGongAn />
+         </div>
        </div>
-     </div>
-
-     {/* 备案 */}
-     <div className='w-full text-center flex flex-wrap items-center justify-center gap-x-2 pb-4'>
-       <BeiAnSite />
-       <BeiAnGongAn />
      </div>
    </footer>
 }
